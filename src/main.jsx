5ffe351cf4987a6bfd3a1572ecf56d07cc9e7fb7
zeethenextbigthing-afk <zeethenextbigthing@gmail.com>
2026-06-14 06:18:56 +0200

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MWPlay from './MWPlay.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MWPlay />
-  </React.StrictMode>
-)
